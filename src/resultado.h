5cf00b47eda5f6dd753c35c223a2b4877e83ccd3
ramirezjpdf <ramirez.jpdf@ymail.com>
2013-11-16 18:10:45 -0200

    adicionando campo tempoTotal no Resultado

--- src/resultado.h
+++ src/resultado.h
@@ -1,9 +1,9 @@
-
 typedef struct _resultado{
-float mediaPI;
-float mediaQuadradoPI;
-int maiorElem;
-int menorElem;
-int maiorPI;
-int menorPI;
+  float mediaPI;
+  float mediaQuadradoPI;
+  int maiorElem;
+  int menorElem;
+  int maiorPI;
+  int menorPI;
+  unsigned int tempoTotal;
 } Resultado;
