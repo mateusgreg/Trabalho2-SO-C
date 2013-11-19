9381b75057dad692a34dd159048d073556bb35f3
Mateus Gregorio <mateusgreg.ufrj@gmail.com>
2013-11-19 08:23:11 -0200

    BUG: Erro de Segmentation Fault ocorrendo no módulo de Threads. Provavelmente o erro está na seção crítica da thread.

--- src/threads.c
+++ src/threads.c
@@ -10,16 +10,21 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <string.h>
 #include "threads.h"
 #include "tarefa.h"
+
+#define DEBUG 1
 
 
 /* Variáveis globais */
 Resultado* resultadoFinal;
+Tarefas* tarefas;
+sem_t* semaphore;
 
 
 pthread_t *alocaVetorThreads(int n){
-	pthread_t *vet = calloc(n, sizeof(pthread_t));
+	pthread_t* vet = calloc(n, sizeof(pthread_t));
 
 	if (vet == NULL) {
 		printf ("Erro de alocacao de memoria do vetor de threads.");
@@ -31,42 +36,85 @@ pthread_t *alocaVetorThreads(int n){
 
 void* processaMatriz(void *threadId) {
 	int tid = (int)threadId;
+	
+	if (DEBUG) printf("Executando código da Thread %d...\n", tid);
 
-	//TODO:executaTarefa(indices, tamVetorIndices);
+	Resultado* resultado = executaTarefa(&tarefas[tid]);	
+	if (DEBUG) printf("Thread %d: Executei Tarefas de %d a %d...\n", tid, tarefas[tid].inicio, tarefas[tid].fim);
 
+
+	sem_wait(semaphore);
+
+	if (DEBUG) printf("Thread %d: Estou na SC...\n", tid);
+
+	resultadoFinal->mediaPI += resultado->mediaPI;
+	resultadoFinal->mediaQuadradoPI += resultado->mediaQuadradoPI;
+	resultadoFinal->maiorPI = (resultadoFinal->maiorPI > resultado->maiorPI) ? resultadoFinal->maiorPI : resultado->maiorPI;
+	resultadoFinal->menorPI = (resultadoFinal->menorPI < resultado->menorPI) ? resultadoFinal->menorPI : resultado->menorPI;
+	resultadoFinal->maiorElem = (resultadoFinal->maiorElem > resultado->maiorElem) ? resultadoFinal->maiorElem : resultado->maiorElem;
+	resultadoFinal->menorElem = (resultadoFinal->menorElem < resultado->menorElem) ? resultadoFinal->menorElem : resultado->menorElem;
+
+	if (DEBUG) printf("tid=%d, maiorElem=%d, menorElem=%d, maiorPI=%d, menorPI=%d, mediaPI = %.4f, mediaQuadradoPI = %.4f\n", tid, resultadoFinal->maiorElem, resultadoFinal->menorElem, resultadoFinal->maiorPI, resultadoFinal->menorPI, resultadoFinal->mediaPI, resultadoFinal->mediaQuadradoPI);
+
+	sem_post(semaphore);
+
+	free(resultado);
 }
 
 Resultado* threads(int nThreads, int k) {
-
-	int //nThreads = k,	//TODO: Definir logica para escolher num. de threads
-		t,
-		indice=0,
-		quociente = k/nThreads,
-		resto = k % nThreads;
-
-	Resultado* resultado;
+	puts("Início do módulo de Threads...");
 	
-	pthread_t *threads;
-	//TODO:Criar semáforos
+	int t;
 
-	//TODO:Inicializar Semáforos
-	threads = alocaVetorThreads(nThreads);
+	tarefas = divideTarefas(nThreads);
 
-	//Cria as threads
+	resultadoFinal = (Resultado*)malloc(sizeof(Resultado));
+	resultadoFinal->PI = (int*)malloc(k * sizeof(int));
+
+	puts("resultadoFinal OK!");
+
+	sem_init(&semaphore, 0, 1);
+
+	puts("Semáforo OK!");
+
+	pthread_t* threads = alocaVetorThreads(nThreads);
+	puts("Aloquei Vetor de Threads...");
+
+	//Cria e inicia as threads
 	for(t=0; t<nThreads; t++) {
 		if (pthread_create(&threads[t], NULL, processaMatriz, (void *)t)) {
-			printf("--ERRO: pthread_create()\n");
+			printf("ERRO: pthread_create()\n");
 			exit(-1);
 		}
+
+		if (DEBUG) printf("Thread %d iniciando...\n", t);
 	}
+
+	puts("Thread Principal: Iniciei todas as Threads");
 
 	//Espera o termino de todas as threads
 	for(t=0; t<nThreads; t++) {
 		if (pthread_join(threads[t], NULL)) {
-			printf("--ERRO: pthread_join() \n");
+			printf("ERRO: pthread_join()\n");
 			exit(-1);
 		}
 	}
+
+	Resultado* resultado = (Resultado*)malloc(sizeof(Resultado));
+	resultado->mediaPI = resultadoFinal->mediaPI;
+	resultado->mediaQuadradoPI = resultadoFinal->mediaQuadradoPI;
+	resultado->maiorPI = resultadoFinal->maiorPI;
+	resultado->menorPI = resultadoFinal->menorPI;
+	resultado->maiorElem = resultadoFinal->maiorElem;
+	resultado->menorElem = resultadoFinal->menorElem;
+	memcpy(resultado->PI, resultadoFinal->PI, sizeof(resultadoFinal->PI));
+
+	puts("Encerrando Módulo de Threads...");
+
+	free(threads);
+	free(semaphore);
+	free(resultadoFinal->PI);
+	free(resultadoFinal);
 
 	return resultado;
 }
